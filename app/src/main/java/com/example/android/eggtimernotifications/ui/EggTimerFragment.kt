f36d28e6d3829ae791ac93a6e82a52f9122777ce
Ítalo Bezerra <italobezerra.cw@gmail.com>
2022-09-27 07:54:45 -0300

    Notification topic was added

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -60,6 +60,7 @@ class EggTimerFragment : Fragment() {
             getString(R.string.breakfast_notification_channel_name)
         )
 
+        subscribeTopic()
         return binding.root
     }
 
@@ -87,6 +88,18 @@ class EggTimerFragment : Fragment() {
 
             notificationManager.createNotificationChannel(notificationChannel)
         }
+    }
+    private fun subscribeTopic() {
+        // [START subscribe_topics]
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.message_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.message_subscribe_failed)
+                }
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+            }
+        // [END subscribe_topics]
     }
 
     companion object {
